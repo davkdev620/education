17ccec717fd75605572db0dc0c41f9fce6ce8192
david <davidkost26@gmail.com>
2022-06-05 23:54:51 +0300

    feat: Accessing the course contents Rendering different types of content

--- education/settings.py
+++ education/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
+    'embed_video',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Setting used by the auth module to redirect the student after a successful
+# login if no next parameter is present in the request.
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- students/views.py
+++ students/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import authenticate, login
 from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
+from django.views.generic.list import ListView
+from courses.models import Course
+from django.views.generic.detail import DetailView
 
 
 class StudentRegistrationView(CreateView):
@@ -38,3 +41,38 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     def get_success_url(self):
         return reverse_lazy('student_course_detail',
                             args=[self.course.id])
+
+
+# displaying the courses that students are enrolled on.
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    # Limit the base QuerySet to courses on which the student is enrolled.
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    # Set a course module in the context if the module_id URL parameter is given.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get course object.
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # Get current module.
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id'])
+        else:
+            # Get first module.
+            context['module'] = course.modules.all()[0]
+        return context
